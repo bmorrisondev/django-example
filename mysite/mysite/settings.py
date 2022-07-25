da8ad634ae6bebcb64c7be7f5164468ef5a0adf6
Brian Morrison II <brian@brianmorrison.me>
2022-07-25 17:04:11 -0500

    Permit host

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -27,7 +27,9 @@ SECRET_KEY = 'akdjfhadlkfhyalksjdh'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'bmorrison-ps-django-example.herokuapp.com'
+]
 
 
 # Application definition
